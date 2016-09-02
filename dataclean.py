a1701aaff9a1c5d5cabfcaaa46e9d4e8a80bcf58
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-09-01 18:26:50 -0600

    add the function about clean data

--- dataclean.py
+++ dataclean.py
@@ -2,7 +2,7 @@
 # @Author: Lich_Amnesia
 # @Email: alwaysxiaop@gmail.com
 # @Date:   2016-09-01 17:11:47
-# @Last Modified time: 2016-09-01 18:11:56
+# @Last Modified time: 2016-09-01 18:23:33
 # @FileName: dataclean.py
 
 import pandas as pd
@@ -16,12 +16,26 @@ def calNan(data_list):
             res += 1
     return res
 
+# solve attendance column data, 
+# return a list
 def solveAttendance(data_list):
-    print(calNan(data_list))
+    res_list = []
+    for i in data_list:
+        if i == i:
+            if 'Not' in i:
+                res_list.append(-1)
+            else: 
+                res_list.append(1)
+        else:
+            res_list.append(0)
+    # print(calNan(data_list))
+    # print res_list
+    return res_list
 
 def main():
-    data = pd.read_csv('Data\\train.csv')
-    solveAttendance(data['attendance'])
+    data = pd.read_csv("Data\\train.csv")
+    attendance_list = solveAttendance(data['attendance'])
+    return attendance_list
 
 if __name__ == '__main__':
     main()
---
+++ init.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Lich_Amnesia
+# @Email: alwaysxiaop@gmail.com
+# @Date:   2016-09-01 18:22:44
+# @Last Modified time: 2016-09-01 18:22:52
+# @FileName: init.py
